382964bad2462f3a08833db09d23b5531de85123
Zid Visda <rashidvisda@gmail.com>
2025-08-09 19:54:21 +0800

    edited .env file

--- frontend/src/context/TodoContext.jsx
+++ frontend/src/context/TodoContext.jsx
@@ -1,14 +1,10 @@
 import React from "react";
 import { createContext, useContext, useState, useEffect } from "react";
 import useFetch from "../Hook/useFetch";
-
+const API_BASE_URL = import.meta.env.VITE_API_BASE_URL;
 const TodoContext = createContext();
 export const TodoProvider = ({ children }) => {
-  const {
-    data: todos,
-    loading,
-    error,
-  } = useFetch("http://localhost:8000/api/todos");
+  const { data: todos, loading, error } = useFetch(`${API_BASE_URL}/api/todos`);
   const [activeTab, setActiveTab] = useState("all");
 
   return (
